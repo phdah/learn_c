4f966a0317f221ec35ce086df2684446e937052c
Philip <philip.dahlqvist-sjoberg@budbee.com>
2022-11-25 15:01:42 +0100

    Optimiz vowel subtraction

--- code_wars/vowel_trolls.c
+++ code_wars/vowel_trolls.c
@@ -1,40 +1,38 @@
 // https://www.codewars.com/kata/52fba66badcd10859f00097e/c
 #include <stdio.h> // printf
-#include <stdlib.h> // atoi
 
 char string[10]="Hello End";
 
-int valueInArray(int value, int* vowlArray) {
+int isVowel(const char c, const int* array) {
 
-  for ( int i = 0; i < 10; i++ )
-    if ( vowlArray[i]==value )
-      return 0;
-
-  return 1;
+  return (c > 96 ? array[c-97] : array[c-65]);
 
 }
 
 int main() {
 
   // all letters
-  int array[10]={65};
+  int array[26]={0};
 
   // all vowls
-  array[1] += 65+4;
-  array[2] += 65+8;
-  array[3] += 65+14;
-  array[4] += 65+20;
-  array[5] += 65+32;
-  array[6] += 65+32+4;
-  array[7] += 65+32+8;
-  array[8] += 65+32+14;
-  array[9] += 65+32+20;
+  array[0] = 1;
+  array[4] = 1;
+  array[8] = 1;
+  array[14] = 1;
+  array[20] = 1;
 
   for ( int i = 0; i < sizeof(string); i++ ) {
-    if ( valueInArray(string[i], array) )
+    if ( string[i] > 64 && string[i] < 91 || string[i] > 96 && string[i] < 123 ) {
+
+      if ( !isVowel(string[i], array) )
+        printf("%c", string[i]);
+
+    } else {
       printf("%c", string[i]);
 
+    }
   }
   printf("\n");
 
+  return 0;
 }
