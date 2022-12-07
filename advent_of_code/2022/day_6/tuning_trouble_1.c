a361be55617a9ba1a5e24b831f1d81cadeea275e
Philip <philip.dahlqvist-sjoberg@budbee.com>
2022-12-07 11:39:52 +0100

    Advent of Code, 2022, day 6 solved

---
+++ advent_of_code/2022/day_6/tuning_trouble_1.c
@@ -0,0 +1,47 @@
+// https://adventofcode.com/2022/day/6
+#include <stdio.h>
+#include <stdlib.h>
+
+#define n 14 // the number of values to check for
+#define m 26 // number of letters
+
+int main(int argc, char** argv)
+{
+
+  FILE* ptr = fopen(argv[1], "r");
+  char str[5000];
+
+  int check[m]={0};
+  int curr;
+
+  int i=0;
+  int k=0;
+  int duplicate=0;
+
+  while ( fgets(str, 5000, ptr) != NULL ) {
+    while ( str[i] != '\n' ) {
+      if ( i > (n-2) ) {
+        for ( int j=0; j < n; j++ ) {
+          check[str[i-j]-97] += str[i-j]-97 + m;
+        }
+        for ( int j=0; j < n; j++ ) {
+          if ( check[str[i-j]-97] > m*2 ) {
+            ++duplicate;
+          }
+          check[str[i-j]-97]=0;
+        }
+        if ( !duplicate ) {
+          printf("Found %d unique values after: %d\n", n, i+1);
+          return 0;
+        }
+        duplicate=0;
+      }
+      ++i;
+    }
+  }
+
+
+
+  fclose(ptr);
+  return 0;
+}
